676288496102b5f06a9705eebe60216802690c92
Jason-Choi-SKKU <jasonchoi3@naver.com>
2021-08-28 00:17:13 +0900

    [ADD] Migrate flask to fastapi && serve nasbench-web

--- app.py
+++ app.py
@@ -1,46 +1,66 @@
-from __future__ import division
+from __future__ import division, print_function, absolute_import
+
+from starlette.responses import RedirectResponse
+
+
 from module.sharpley_values import sharpley_values
 from module.querying import querying, recommend_cell
-import json
-from __future__ import print_function
-from __future__ import absolute_import
+import json, os, requests
 
-from flask import Flask, request
-from flask_restx import Resource, Api, reqparse
-from flask_cors import CORS
 
 from absl import app as absl_app
 from nasbench import api as nasbench_api
 
+
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+
+from pydantic import BaseModel
+from typing import Optional
+
+
+if not os.path.isfile('./nasbench_only108.tfrecord'):
+    print('There is no tfrecord file. Downloading...')
+    req = requests.get('https: // storage.googleapis.com/nasbench/nasbench_only108.tfrecord', allow_redirects=True)
+    open('nasbench_only108.tfrecord', 'wb').write(req.content)
+
+
 NASBENCH_TFRECORD = './nasbench_only108.tfrecord'
 nasbench = nasbench_api.NASBench(NASBENCH_TFRECORD)
 
-app = Flask(__name__)
-CORS(app)
-api = Api(app)
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post('/querying')
+async def get_querying(request: Request):
+    body = request.body()
+    input_matrix = body.get('matrix')
+    ops = body.get('ops')
+    return querying(nasbench_api, nasbench, input_matrix, ops)
 
 
-@api.route('/querying')
-class Querying(Resource):
-    def post(self):
-        input_matrix = request.json.get('matrix')
-        ops = request.json.get('ops')
-        return querying(nasbench_api, nasbench, input_matrix, ops)
+@app.post('/recommendation')
+async def get_recommend_cell(request: Request):
+    body = request.body()
+    edge_data = body.get('edge_data')
+    node_data = body.get('node_data')
+    return recommend_cell(nasbench, edge_data, node_data)
 
+@app.get('/overview/edge-sharpley-value')
+async def get_edge_sharpley_value():
+    return sharpley_values
 
-@api.route('/recommendation')
-class Recommendation(Resource):
-    def post(self):
-        edge_data = request.json.get('edge_data')
-        node_data = request.json.get('node_data')
-        return recommend_cell(nasbench, edge_data, node_data)
-
-
-@api.route('/overview/edge-sharpley-value')
-class EdgeSharpleyValue(Resource):
-    def get(self):
-        return sharpley_values
-
-
-if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
+@app.get('/')
+async def index():
+    return RedirectResponse('/static/index.html', 307)
